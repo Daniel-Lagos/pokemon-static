470edd31d398608ba6a43bbed5b33c2511bcc6ea
Daniel Lagos <d.f.l.m.0126@gmail.com>
2022-04-09 16:55:35 -0500

    add Link to navbar

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,24 +1,34 @@
-import { Spacer, useTheme,Text } from "@nextui-org/react"
-import { FC } from "react"
+import NextLink from 'next/link';
+import { Spacer, useTheme, Text, Link } from '@nextui-org/react';
+import { FC } from 'react';
 
-export const Navbar:FC=()=> {
+export const Navbar: FC = () => {
 
-  const {theme} = useTheme();
+  const { theme } = useTheme();
 
   return (
     <div style={{
-        display: 'flex',
-        width: '100%',
-        alignItems: 'center',
-        justifyContent: 'start',
-        padding: '0px 20px',
-        backgroundColor: theme?.colors.gray900.value
+      display: 'flex',
+      width: '100%',
+      alignItems: 'center',
+      justifyContent: 'start',
+      padding: '0px 60px',
+      backgroundColor: theme?.colors.gray900.value
     }}>
-        <Text color='white' h2>P</Text>
-        <Text color='white' h3>okemon</Text>
+      <NextLink href={'/'} passHref>
+        <Link>
+          <Text color="white" h2>P</Text>
+          <Text color="white" h3>okemon</Text>
+        </Link>
+      </NextLink>
 
-        <Spacer css={{flex: 1}}/>
-        <Text h3>Favoritos</Text>
+      <Spacer css={{ flex: 1 }}/>
+
+      <NextLink href={'/favorites'} passHref>
+        <Link>
+          <Text h3>Favoritos</Text>
+        </Link>
+      </NextLink>
     </div>
-  )
-}
+  );
+};
---
+++ pages/favorites/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next';
+import { Layout } from '../../components/layouts';
+
+
+const FavoritesPage: NextPage = () => {
+  return (
+    <Layout title={'favorites'}>
+      <h1>Favorites Page</h1>
+    </Layout>
+  );
+};
+
+export default FavoritesPage;
